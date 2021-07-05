1c663b0277dc61d8e2ca65df4ec9dd7d7650b656
TTTT55 <iamtttt55@gmail.com>
2021-07-05 10:08:13 +0000

    Revert "ShapeShifter: Utilize AppListPreference with SensorBlock & GamingMode"
    This reverts commit 7452a07e6cd55d871b470a75de068c59a6e1a6f3.

--- src/com/ssos/shapeshifter/fragments/system_misc/SensorBlockSettings.java
+++ src/com/ssos/shapeshifter/fragments/system_misc/SensorBlockSettings.java
@@ -49,7 +49,6 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 
-import com.ssos.support.preferences.AppListPreference;
 import com.ssos.support.preferences.PackageListAdapter;
 import com.ssos.support.preferences.PackageListAdapter.PackageItem;
 
@@ -254,7 +253,7 @@ public class SensorBlockSettings extends SettingsPreferenceFragment
         PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
                 PackageManager.GET_META_DATA);
         Preference pref =
-                new AppListPreference(getActivity());
+                new Preference(getActivity());
 
         pref.setKey(pkg.name);
         pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
